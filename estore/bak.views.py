b745ed672452c44c3a30f2b040d4ad33aa8df9be
Hank <j.h.jiang.hank@gmail.com>
2017-08-05 14:33:29 +0800

    Week1-Homework

--- estore/bak.views.py
+++ estore/bak.views.py
@@ -10,6 +10,11 @@ from .models import Product
 
 # Create your views here.
 
+"""
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'estore/product_list.html', {'products': products})
+"""
 #class ProductList(generic.ListView):
 class ProductList(PermissionRequiredMixin, generic.ListView):
     model = Product
@@ -18,11 +23,24 @@ class ProductList(PermissionRequiredMixin, generic.ListView):
         if self.permission_required:
             return super(ProductList, self).has_permission()
         else:
-            return True
+            return Ture
 
-class ProductDetail(generic.DetailView):
+class ProductDetail(generic.DetailView)
     model = Product
 
+
+"""
+@permission_required('estore.add_product')
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'estore/product_form.html', {'form': form})
+"""
 class ProductCreate(PermissionRequiredMixin, generic.CreateView):
     permission_required = 'estore.add_product'
     model = Product
@@ -33,20 +51,33 @@ class ProductCreate(PermissionRequiredMixin, generic.CreateView):
         #return reverse('product_list')
         return reverse('dashboard_product_list')
 
+"""
+@permission_required('esotre.change_product')
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    form = ProductForm(request.POST or None, isinstance=prodcut)
+
+    if request.POST and form.is_valid():
+        form.save()
+        messages.success(request, '產品已變更')
+
+    return render(request, 'esostre/product_form.html', {'form': form})
+"""
 class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'estore.change_product'
     model = Product
-    fields = ('title', 'description', 'quantity', 'price', 'image')
+    fields = ('title', 'description', 'quantity', 'prise', 'image')
 
     def get_success_url(self):
         messages.success(self.request, '產品已變更')
         #return reverse('product_update', kwargs=self.kwargs)
         return reverse('dashboard_product_update', kwargs=self.kwargs)
 
-class UserList(PermissionRequiredMixin, generic.ListView):
+    class UserList(PermissionRequiredMixin, generic.ListView):
     permission_required = 'auth.change_user'
     model = User
     template_name = 'estore/dashboard_user_list.html'
+
 
 class UserAddToStaff(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'auth.change_user'
@@ -59,6 +90,7 @@ class UserAddToStaff(PermissionRequiredMixin, generic.UpdateView):
             group.user_set.add(self.object)
             messages.success(self.request, '已變更使用者身份為管理者')
         return reverse('dashboard_user_list')
+
 
 class UserRemoveFromStaff(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'auth.change_user'
--- estore/models.py
+++ estore/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=255, verbose_name='產品名稱')
-    description = models.TextField(verbose_name='產品名稱')
+    description = models.TextField(verbose_name='產品描述')
     quantity = models.IntegerField(verbose_name='庫存數量')
     price = models.IntegerField(verbose_name='價格')
+    image = models.ImageField(upload_to='product_images/', verbose_name='商品圖片', blank=True, null=True)
 
     def __str__(self):
         return self.title
